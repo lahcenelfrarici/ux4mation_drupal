328d3fab77cbc0e7afe35d19defdaed43161946f
Lahcen Elfarici <lahcen@fornet.ma>
2024-09-21 17:46:40 +0100

    reglé responsive all pages and add sql

--- themes/custom/ux4mation/js/index_el.js
+++ themes/custom/ux4mation/js/index_el.js
@@ -277,7 +277,24 @@
       }
     }
   });
-
+  $('.brand-carouselhow').owlCarousel({
+    dots: false,
+    autoplayTimeout: 700,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  })
   let targetNumber = 30; // Set the target number you want to reach
   let speed = 50; // Set the speed of the counter (in milliseconds)
 
